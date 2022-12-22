ab6d18296bb60c1556b019952e01547085a07fe3
Aaron Matson <matsonaaron1@gmail.com>
2022-12-22 14:17:29 -0700

    Added Images, Data. Starting build of App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css'
 
 const App = () => {
+
+  const activeMenu = true;
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter >
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background: 'blue', borderRadius: '50%'}}>
+                <FiSettings size={24} />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div>
+              Sidebar
+            </div>
+          ): (
+            <div>
+              Sidebar w-0
+            </div>
+          )}
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
